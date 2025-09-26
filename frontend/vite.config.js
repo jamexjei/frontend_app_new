26d42cc95653b92ec5fe11dab00e52079cd1765d
jeison castro <97313659+jamexjei@users.noreply.github.com>
2025-09-26 11:33:36 -0500

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,1 +1,15 @@
 
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// ⚡ configuración de Vite
+export default defineConfig({
+  plugins: [vue()],
+  server: {
+    port: 5173,
+    proxy: {
+      // Proxy para redirigir peticiones API al backend en FastAPI
+      '/api': 'http://localhost:8000'
+    }
+  }
+})
